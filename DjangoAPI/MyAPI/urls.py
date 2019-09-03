6f9be20552ab0ae10ac9434407ef9d549cc016de
siddhant3030 <ssiddhant3030@gmail.com>
2019-09-03 14:16:09 +0530

    added serializer and forms for the api

--- DjangoAPI/MyAPI/urls.py
+++ DjangoAPI/MyAPI/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('MyAPI', views.ApprovalsView)
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    
-]
+	path('form/', views.myform, name='myform'),
+    path('api/', include(router.urls)),
+    path('status/', views.approvereject),
+ 
+] 
